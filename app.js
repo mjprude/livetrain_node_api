f01bd33d82db793e0ed62065e7a71a25c6167b07
mjprude <michaelprude@gmail.com>
2017-02-25 10:36:17 -0500

    Make data look more like v1 api

--- app.js
+++ app.js
@@ -4,10 +4,57 @@ const app = express()
 
 app.get('/', function (req, res) {
   mtaJsonFeedMessage(1).then((data) => {
-    res.send(data)
+    res.send(massageData(data));
   });
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+function massageData(data) {
+  return data.entity.map(update => {
+    const { trip_update } = update || {};
+
+    if (!trip_update) {
+      return;
+    }
+
+    const {
+      trip: {
+        trip_id,
+        route_id
+      } = {},
+      stop_time_update
+    } = trip_update;
+
+    if (!stop_time_update) {
+      return;
+    }
+
+    const routeIdentifier = trip_id.split('..')[1];
+    const direction = routeIdentifier ? routeIdentifier[0] : '';
+    const updated = Math.floor(new Date().getTime() / 1000);
+
+    const stopTimeUpdate = stop_time_update.map(_massageStopTimes);
+
+    return {
+      trip_id: trip_id.split('_')[0],
+      route: route_id,
+      direction,
+      updated,
+      stopTimeUpdate
+    };
+  }).filter(e => e);
+}
+
+function _massageStopTimes(stopTime) {
+  const {
+    stop_id: stopId,
+    arrival: arrivalObj,
+    departure: departureObj
+  } = stopTime || {};
+  const { time: { low: arrival = null } = {} } = arrivalObj || {};
+  const { time: { low: departure = null } = {} } = departureObj || {};
+  return { stopId, arrival, departure };
+}
