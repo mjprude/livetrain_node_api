8a42adfc6fb93453d09cc0767aa5dd5c6632fa42
mjprude <michaelprude@gmail.com>
2017-08-13 09:32:52 -0400

    POC implementation of v1 API

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
-const mtaJsonFeedMessage = require('./utils/mtaFeed');
+const mtaJsonFeedMessage = require('./utils/fetchFeed');
+const mtaStatic = require('./utils/mtaStatic');
 const app = express()
+const _ = require('lodash');
+let trips = {};
 
-app.get('/', function (req, res) {
-  mtaJsonFeedMessage(1).then((data) => {
+function fullUpdate(req, res) {
+  Promise.all([
+    mtaJsonFeedMessage(1).then(data => massageData(data)),
+    mtaJsonFeedMessage(2).then(data => massageData(data))
+  ]).then((data) => {
+    res.send(Array.prototype.concat.apply([], data));
+  }).catch((error) => res.send(error));
+}
+
+function getLine(req, res) {
+  const line = mtaStatic.feedId[req.params.line];
+  mtaJsonFeedMessage(line).then((data) => {
     res.send(massageData(data));
   });
-})
+}
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
 })
 
 function massageData(data) {
@@ -26,7 +39,7 @@ function massageData(data) {
         route_id
       } = {},
       stop_time_update
-    } = trip_update;
+    } = trip_update || {};
 
     if (!stop_time_update) {
       return;
@@ -36,14 +49,40 @@ function massageData(data) {
     const direction = routeIdentifier ? routeIdentifier[0] : '';
     const updated = Math.floor(new Date().getTime() / 1000);
 
-    const stopTimeUpdate = stop_time_update.map(_massageStopTimes);
+    const stopTimeUpdate = stop_time_update.map(_massageStopTimes) || [];
+
+    const [
+      {
+        stopId: stop1,
+        arrival: arrival1,
+        departure: departure1,
+      } = {},
+      {
+        stopId: stop2,
+        arrival: arrival2,
+        departure: departure2,
+      } = {},
+      {
+        stopId: stop3,
+        arrival: arrival3,
+        departure: departure3,
+      } = {}
+    ] = _updateTrips(trips, trip_id, stopTimeUpdate, updated) || [];
 
     return {
       trip_id: trip_id.split('_')[0],
       route: route_id,
       direction,
       updated,
-      stopTimeUpdate
+      stop1,
+      arrival1,
+      departure1,
+      stop2,
+      arrival2,
+      departure2,
+      stop3,
+      arrival3,
+      departure3
     };
   }).filter(e => e);
 }
@@ -58,3 +97,38 @@ function _massageStopTimes(stopTime) {
   const { time: { low: departure = null } = {} } = departureObj || {};
   return { stopId, arrival, departure };
 }
+
+function _updateTrips(trips, tripId, stopTimeUpdate, updated) {
+  const cachedTrip = trips[tripId];
+  if (!cachedTrip || _.isEqual(cachedTrip, stopTimeUpdate)) {
+    trips[tripId] = stopTimeUpdate;
+    return stopTimeUpdate.slice(0, 3);
+  }
+
+  const oldStops = cachedTrip.filter(trip => {
+    return (trip.arrival < updated || trip.departure < updated);
+  });
+  const allStops = oldStops.concat(stopTimeUpdate);
+  trips[tripId] = allStops;
+  const lastStop = oldStops[oldStops.length - 1];
+  const threeStops = lastStop ?
+    [lastStop].concat(stopTimeUpdate.slice(0, 2)) :
+    stopTimeUpdate.slice(0, 3);
+  return threeStops || [];
+}
+
+// Versioning
+const v1 = express.Router();
+const v2 = express.Router();
+
+v1.use('/lines', express.Router()
+  .get('/:line', getLine));
+
+v1.use('/', express.Router().get('/', fullUpdate));
+
+v2.use('/lines', express.Router()
+  .get('/:line', getLine));
+
+app.use('/v1', v1);
+app.use('/v2', v2);
+app.use('/', v1); // Set the default version to latest.
---
+++ utils/mtaStatic.js
@@ -0,0 +1,24 @@
+/**
+ * feedId
+ * A mapping of MTA line name to their corresponding feed. Note that multiple
+ * lines may be grouped under the same feed.
+ * See http://datamine.mta.info/list-of-feeds for more details
+ */
+exports.feedId = {
+  1: 1,
+  2: 1, 
+  3: 1,
+  4: 1,
+  5: 1,
+  6: 1,
+  S: 1,
+  C: 26,
+  B: 21,
+  D: 21,
+  N: 16,
+  Q: 16,
+  R: 16,
+  W: 16,
+  L: 2,
+  SIR: 11
+};
